c60eef1c746e688c7bf9b753d044d1b755b43338
itsamejms <james.twose@alliander.com>
2026-03-10 19:32:35 +0000

    removing unused pages/ components and doing a bit of SEO

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import Admin from "@/pages/Admin";
 import ForgottenPassword from "@/pages/auth/ForgottenPassword";
 import Login from "@/pages/auth/Login";
 import Register from "@/pages/auth/Register";
@@ -8,20 +7,15 @@ import Dashboard from "@/pages/Dashboard";
 import SmartUrlDashboard from "@/pages/dashboard/SmartUrlDashboard";
 import Help from "@/pages/Help";
 import Index from "@/pages/Index";
-import LearnMore from "@/pages/LearnMore";
 import CreateCampaign from "@/pages/CreateCampaign";
 import NotFound from "@/pages/NotFound";
 import PaymentFailed from "@/pages/PaymentFailed";
 import PaymentSuccess from "@/pages/PaymentSuccess";
-import PlaylistTips from "@/pages/playlist/PlaylistTips";
-import PlaylistChecker from "@/pages/PlaylistChecker";
 import Playlists from "@/pages/Playlists";
 import Pricing from "@/pages/Pricing";
 import Privacy from "@/pages/Privacy";
 import Settings from "@/pages/Settings";
 import Terms from "@/pages/Terms";
-import Feedback from "@/pages/Feedback";
-import Submission from "@/pages/Submission";
 import PaymentCreditsPage from "@/pages/PaymentCredits";
 import AuthGuard from "@/components/AuthGuard";
 import TestimonialsPage from "./pages/TestimonialsPage";
@@ -39,17 +33,12 @@ export default function AppRoutes() {
         <Route path="/forgotten-password" element={<ForgottenPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route path="/smart-url" element={<SmartUrl />} /> */}
         <Route path="/blog" element={<Blog />} />
-        {/* <Route path="/learn-more" element={<LearnMore />} /> */}
         <Route path="/help" element={<Help />} />
-        {/* <Route path="/playlist-checker" element={<PlaylistChecker />} /> */}
-        {/* <Route path="/playlist-tips" element={<PlaylistTips />} /> */}
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
-        <Route path="/feedback" element={<Feedback />} />
-        {/* <Route path="/submissions" element={<Submission />} /> */}
+        {/* <Route path="/feedback" element={<Feedback />} /> */}
         <Route path="/campaigns/new" element={<CreateCampaign />} />
         <Route path="/payment-credits" element={<PaymentCreditsPage />} />
         <Route path="/payment-success" element={<PaymentSuccess />} />
@@ -61,7 +50,6 @@ export default function AppRoutes() {
         <Route path="/dashboard" element={<AuthGuard><Dashboard /></AuthGuard>} />
         <Route path="/dashboard/smart-url" element={<AuthGuard><SmartUrlDashboard /></AuthGuard>} />
         <Route path="/playlists" element={<AuthGuard><Playlists /></AuthGuard>} />
-        <Route path="/admin" element={<AuthGuard><Admin /></AuthGuard>} />
         <Route path="/settings" element={<AuthGuard><Settings /></AuthGuard>} />
 
         {/* Catch-all */}
